bb38d3f60bc8b1a6ff686d32b5849340d48287a3
rocking42 <nedpike3@gmail.com>
2017-01-11 13:35:16 +1100

    Modified CSS grid and added rainbow and panorama plugins

---
+++ js/rainbow.js
@@ -0,0 +1,18 @@
+$.fn.rainbowText = function () {
+    for (var _i = 0, _a = this; _i < _a.length; _i++) {
+        var item = _a[_i];
+        var letters = item.innerText.split("");
+        item.innerText = "";
+        var count = 0;
+        var results = [];
+        for (var _b = 0, letters_1 = letters; _b < letters_1.length; _b++) {
+            var letter = letters_1[_b];
+            var color = count * (Math.random() * 255 / letters.length);
+            var stuff = document.createElement("span");
+            stuff.innerText = letter;
+            stuff.style.color = "hsl(" + color + ", 40%, 50%)";
+            item.append(stuff);
+            count++;
+        }
+    }
+};
