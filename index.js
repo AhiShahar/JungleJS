474a4c13cbcf5223fb80776a43e0324377e4b8c5
rocking42 <nedpike3@gmail.com>
2017-01-13 03:23:25 +1100

    Added buttons and typography

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Determines urlRoute based on whether local or hosted
 function urlRoute() {
   if (window.location.href.includes("file") || window.location.href.includes("local")) {
+    // Find the url in the data-url passed into the html element
     window.location.pathname = this.dataset.url;
   } else if (window.location.href.includes("ahishahar")) {
     window.location.href = `/JungleJS/${this.dataset.url}`;
